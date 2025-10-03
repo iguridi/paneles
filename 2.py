05d48ac3201ec5ec1e3a75228fb034a57906780d
iguridi <iguridi@users.noreply.github.com>
2025-10-03 11:07:06 -0700

    frontend

--- 2.py
+++ 2.py
@@ -1,9 +1,27 @@
 import streamlit as st
 import pandas as pd
 
+
+st.file_uploader("paneles.csv")
+
+opcion = st.radio("Seleccione que listado desea detallar", [
+"Despiece detallado",
+"Materia prima necesaria por perfil (incluye totales)",
+"Soldadura necesaria por panel",
+"Tiempos por panel",
+"Costos por panel (con USD/m² + resumen)",
+"Detalle de insumos por pieza y total pedido",
+"Área por panel",
+"Resumen",
+"Todos",
+])
+
+generar = st.button("Generar")
+
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
-df
+# df
